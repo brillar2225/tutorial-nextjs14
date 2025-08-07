488cbf42de3af4252a9d3e897658e6deb7d0e0ce
Jaehee Chung <brillar2225@gmail.com>
2025-08-07 18:41:59 +0900

    Add fetching videos in movie detail

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -34,17 +34,43 @@ async function getMovie(id: string) {
   return json;
 }
 
+async function getVideos(id: string) {
+  const response = await fetch(`${BASE_URL}/${id}/videos`);
+  const json = await response.json();
+  return json;
+}
+
 export default async function MovieDetail({
   params
 }: {
   params: Promise<{ id: string }>
 }) {
   const { id } = await params;
-  const movie = await getMovie(id);
+  
+  /** 
+   * MEMO:
+   * ```tsx
+   * const movie = await getMovie(id);
+   * const videos = await getVideos(id);
+   * ```
+   * 위와 같이 비동기 함수를 호출하면 순차적으로 실행되기 때문에 movie가 완료되기까지 videos는 대기하게 됨.
+   * 따라서 아래의 Promise.all과 같이 동시에 실행되도록 최적화 해야함.
+  */
+  const [movie, videos] = await Promise.all([
+    getMovie(id),
+    getVideos(id)
+  ]);
 
   return (
     <div>
       <h1>{movie.title}</h1>
+      <ul>
+        {videos.map((video) => (
+          <li key={video.id}>
+            <a href={`https://www.youtube.com/watch?v=${video.key}`}>{video.name}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
